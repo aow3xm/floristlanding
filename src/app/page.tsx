7d399e8c0d9e76739a525d74c0bbeec4d414713f
aow3xm <149279753+aow3xm@users.noreply.github.com>
2024-06-17 19:50:42 +0700

    feat(header): add dynamic CSS classes using clsx
    Implement dynamic CSS classes in the Header component using the clsx library. This feature allows the header to change its style based on the scroll position, improving the user interface experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { HeroImage, IndoorSVG, OutdoorSVG, CactusSVG } from "../../public";
 export default function Home() {
   return (
     <main className="space-y-20">
+      {/* -------------------------------------------------------------------------------------------------------------------- */}
       <section className="h-screen relative">
         <Image
           // style={{ filter: "blur(5px)" }}
@@ -29,7 +30,7 @@ export default function Home() {
             <button className="bg-green-900 hover:bg-green-950 hover:-translate-y-1 duration-100 px-10 py-2 rounded">
               Book Now
             </button>
-            <button className="hover:-translate-y-1 duration-100 px-10 py-2 border rounded flex flex-row gap-2 justify-center">
+            <button className="hover:-translate-y-1 duration-100 px-10 py-2 border rounded flex flex-row gap-2 justify-center items-center">
               <CirclePlay />
               Watch Video
             </button>
@@ -37,8 +38,14 @@ export default function Home() {
         </div>
       </section>
       {/* -------------------------------------------------------------------------------------------------------------------- */}
-      <section className="container mx-auto flex flex-col lg:flex-row justify-evenly gap-5 lg:gap-0">
-        <div className="h-full lg:h-96 w-full lg:w-96 bg-white p-10 border rounded-xl drop-shadow-md space-y-5 text-green-900">
+
+
+
+
+
+      {/* -------------------------------------------------------------------------------------------------------------------- */}
+      <section className="container mx-auto flex flex-col lg:flex-row justify-between gap-5">
+        <div className="h-full lg:h-96 w-full lg:w-1/3 bg-white p-10 border rounded-xl drop-shadow-md space-y-5 text-green-900">
           <Image src={IndoorSVG} alt="indoor" width={75} height={75} />
           <h3 className="text-2xl font-bold">Indoor Plants</h3>
           <p className="lg:text-lg">
@@ -46,7 +53,7 @@ export default function Home() {
             selection of outdoor plants
           </p>
         </div>
-        <div className="h-full lg:h-96 w-full lg:w-96 bg-green-700 text-white p-10 border rounded-xl drop-shadow-md space-y-5 ">
+        <div className="h-full lg:h-96 w-full lg:w-1/3 bg-green-700 text-white p-10 border rounded-xl drop-shadow-md space-y-5 ">
           <Image src={OutdoorSVG} alt="indoor" width={75} height={75} />
           <h3 className="text-2xl font-bold">Outdoor Plants</h3>
           <p className="lg:text-lg">
@@ -55,7 +62,7 @@ export default function Home() {
             touch to your home.
           </p>
         </div>
-        <div className="h-full lg:h-96 w-full lg:w-96 bg-white p-10 border rounded-xl drop-shadow-md space-y-5 text-green-900">
+        <div className="h-full lg:h-96 w-full lg:w-1/3 bg-white p-10 border rounded-xl drop-shadow-md space-y-5 text-green-900">
           <Image src={CactusSVG} alt="indoor" width={75} height={75} />
           <h3 className="text-2xl font-bold">Plants Pots</h3>
           <p className="lg:text-lg">
@@ -63,6 +70,106 @@ export default function Home() {
             collection of pots plants, available in a variety of sizes and
             designs to fit any decor
           </p>
+        </div>
+      </section>
+      {/* -------------------------------------------------------------------------------------------------------------------- */}
+
+
+      {/* -------------------------------------------------------------------------------------------------------------------- */}
+      <section className='container mx-auto space-y-10'>
+      <h1 className='text-4xl font-bold text-center text-green-900 tracking-tight'>What We Offer To You</h1>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+          <div className="flex flex-col border rounded-lg overflow-hidden cursor-pointer">
+            <div>
+              <Image src='/hero.jpg' alt='hero' height={350} width={350} objectFit="cover"/>
+            </div>
+            <div className='flex justify-between flex-wrap items-center gap-3 p-5 text-green-900 font-semibold'>
+              <div className="flex flex-col">
+                <p className=' text-sm lg:text-base'>Cactus Plant</p>
+                <p className='text-sm'>$8</p>
+              </div>
+              <button
+                  className='border border-green-900 rounded-md px-5 py-1 lg:py-2 hover:bg-green-900 hover:text-white duration-200'>Buy
+                Now
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col border rounded-lg overflow-hidden cursor-pointer">
+            <div>
+              <Image src='/hero.jpg' alt='hero' height={350} width={350} objectFit="cover"/>
+            </div>
+            <div className='flex justify-between flex-wrap items-center gap-3 p-5 text-green-900 font-semibold'>
+              <div className="flex flex-col">
+                <p className=' text-sm lg:text-base'>Cactus Plant</p>
+                <p className='text-sm'>$8</p>
+              </div>
+              <button
+                  className='border border-green-900 rounded-md px-5 py-1 lg:py-2 hover:bg-green-900 hover:text-white duration-200'>Buy
+                Now
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col border rounded-lg overflow-hidden cursor-pointer">
+            <div>
+              <Image src='/hero.jpg' alt='hero' height={350} width={350} objectFit="cover"/>
+            </div>
+            <div className='flex justify-between flex-wrap items-center gap-3 p-5 text-green-900 font-semibold'>
+              <div className="flex flex-col">
+                <p className=' text-sm lg:text-base'>Cactus Plant</p>
+                <p className='text-sm'>$8</p>
+              </div>
+              <button
+                  className='border border-green-900 rounded-md px-5 py-1 lg:py-2 hover:bg-green-900 hover:text-white duration-200'>Buy
+                Now
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col border rounded-lg overflow-hidden cursor-pointer">
+            <div>
+              <Image src='/hero.jpg' alt='hero' height={350} width={350} objectFit="cover"/>
+            </div>
+            <div className='flex justify-between flex-wrap items-center gap-3 p-5 text-green-900 font-semibold'>
+              <div className="flex flex-col">
+                <p className=' text-sm lg:text-base'>Cactus Plant</p>
+                <p className='text-sm'>$8</p>
+              </div>
+              <button
+                  className='border border-green-900 rounded-md px-5 py-1 lg:py-2 hover:bg-green-900 hover:text-white duration-200'>Buy
+                Now
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col border rounded-lg overflow-hidden cursor-pointer">
+            <div>
+              <Image src='/hero.jpg' alt='hero' height={350} width={350} objectFit="cover"/>
+            </div>
+            <div className='flex justify-between flex-wrap items-center gap-3 p-5 text-green-900 font-semibold'>
+              <div className="flex flex-col">
+                <p className=' text-sm lg:text-base'>Cactus Plant</p>
+                <p className='text-sm'>$8</p>
+              </div>
+              <button
+                  className='border border-green-900 rounded-md px-5 py-1 lg:py-2 hover:bg-green-900 hover:text-white duration-200'>Buy
+                Now
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col border rounded-lg overflow-hidden cursor-pointer">
+            <div>
+              <Image src='/hero.jpg' alt='hero' height={350} width={350} objectFit="cover"/>
+            </div>
+            <div className='flex justify-between flex-wrap items-center gap-3 p-5 text-green-900 font-semibold'>
+              <div className="flex flex-col">
+                <p className=' text-sm lg:text-base'>Cactus Plant</p>
+                <p className='text-sm'>$8</p>
+              </div>
+              <button
+                  className='border border-green-900 rounded-md px-5 py-1 lg:py-2 hover:bg-green-900 hover:text-white duration-200'>Buy
+                Now
+              </button>
+            </div>
+          </div>
+
         </div>
       </section>
       {/* -------------------------------------------------------------------------------------------------------------------- */}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,37 +6,41 @@ import styles from "./header.module.css";
 import clsx from "clsx";
 import useScroll from "@/hooks/useScroll";
 export default function Header() {
-  const isScrolled = useScroll();
-  const headerClass = clsx("fixed w-full z-50 py-5 duration-500", {
+  const isScrolled:boolean = useScroll(500);
+  const headerClass:string = clsx("fixed w-full z-50 py-5 duration-200", {
     "bg-black/5 backdrop-blur-md": isScrolled,
+  });
+  const linkClass:string = clsx("hidden md:block space-x-8 font-semibold", {
+    "text-green-900": isScrolled,
   });
   return (
     <header className={headerClass}>
-      <div
+      <nav
         className="container mx-auto flex justify-between items-center text-white"
         style={{ fontFamily: "var(--font-lato)" }}
       >
-        <Link href={paths.home}>
-          <Image src="/brand.svg" alt="Logo" width={50} height={50} />
-        </Link>
-        <div className="hidden md:block space-x-8">
-          <Link className={styles.link} href={paths.home}>
-            Home
+          <Link href={paths.home}>
+            <Image src="/brand.svg" alt="Logo" width={50} height={50} />
           </Link>
-          <Link className={styles.link} href={paths.about}>
-            About Us
-          </Link>
-          <Link className={styles.link} href={paths.contact}>
-            Contact
-          </Link>
-          <Link className={styles.link} href={paths.login}>
-            Login
-          </Link>
-          <button className="border rounded px-4 py-1 hover:scale-105 duration-200">
-            Call Us
-          </button>
+          <div className={linkClass}>
+            <Link className={styles.link} href={paths.home}>
+              Home
+            </Link>
+            <Link className={styles.link} href={paths.about}>
+              About Us
+            </Link>
+            <Link className={styles.link} href={paths.contact}>
+              Contact
+            </Link>
+            <Link className={styles.link} href={paths.login}>
+              Login
+            </Link>
+            <button className="border rounded px-4 py-1">
+              Call Us
+            </button>
         </div>
-      </div>
+
+      </nav>
     </header>
   );
 }
